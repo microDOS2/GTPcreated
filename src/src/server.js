c3e6f03cf0338c4faca462d8c5a8af1ea4976db3
Holt Crowder <microdostoo@gmail.com>
2025-08-13 20:10:43 -0600

    Update server.js

--- src/src/server.js
+++ src/src/server.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     return res.end('OK');
   }
+
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('GTPcreated');
 });
