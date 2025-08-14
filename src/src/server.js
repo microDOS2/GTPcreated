d9dcf3143cda380c6aad619ace56d0ff5b29a305
Holt Crowder <microdostoo@gmail.com>
2025-08-13 20:33:14 -0600

    Update server.js

--- src/src/server.js
+++ src/src/server.js
@@ -1,16 +1,1 @@
-const http = require('http');
-const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/health') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    return res.end('OK');
-  }
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('GTPcreated');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
