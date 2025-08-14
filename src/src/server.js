cf848a6f9aed7777e84f96607491a12736022221
Holt Crowder <microdostoo@gmail.com>
2025-08-13 19:26:10 -0600

    Create server.js

---
+++ src/src/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    return res.end('OK');
+  }
+  res.writeHead
